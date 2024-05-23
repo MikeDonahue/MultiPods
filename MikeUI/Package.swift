9b268e1955fc54217eec278e6002fb9edd56ad77
Mike Donahue <development.md@gmail.com>
2024-05-23 13:50:51 -0400

    Try bundled package file

--- MikeUI/Package.swift
+++ MikeUI/Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "MikeUI",
             targets: ["MikeUI"]),
     ],
+    dependencies: [
+        .package(path: "../MikeCore"),
+        .package(path: "../MikeFoundation"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MikeUI"),
+            dependencies: ["MikeCore", "MikeFoundation"]),
         .testTarget(
             name: "MikeUITests",
             dependencies: ["MikeUI"]),
--- Package.swift
+++ Package.swift
@@ -1,55 +1,27 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "MultiPods",
+    name: "FinalPack",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MikeFoundation",
-            targets: ["MikeFoundation"]),
-        .library(
-            name: "MikeCore",
-            targets: ["MikeCore"]),
-        .library(
-            name: "MikeUI",
-            targets: ["MikeUI"]),
-        .library(
-            name: "MikeBundled",
-            targets: ["MikeFoundation", "MikeCore", "MikeUI"]),
+            name: "FinalPack",
+            targets: ["FinalPack"]),
+    ],
+    dependencies: [
+        .package(path: "../UI"),
+        .package(path: "../Core"),
+        .package(path: "../Foundation"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-
-        // Foundation
         .target(
-            name: "MikeFoundation"),
-            path: "MikeFoundation/MikeFoundation/",
+            name: "FinalPack",
+            dependencies: ["UI", "Core", "Foundation"]),
         .testTarget(
-            name: "MikeFoundationTests",
-            path: "MikeFoundation/MikeFoundationTests/",
-        
-        // Core
-        .target(
-            name: "MikeCore"),
-            dependencies: ["MikeFoundation"],
-            path: "MikeCore/MikeCore/",
-        .testTarget(
-            name: "MikeCoreTests",
-            dependencies: ["MikeFoundation"],
-            path: "MikeCore/MikeCoreTests/",
-
-        // UI
-        .target(
-            name: "MikeUI"),
-            dependencies: ["MikeFoundation", "MikeCore"],
-            path: "MikeUI/MikeUI/",
-        .testTarget(
-            name: "MikeUITests",
-            dependencies: ["FirebaseAnalyticsSwift", "MikeCore"],
-            path: "MikeUI/MikeUITests/",
+            name: "FinalPackTests",
+            dependencies: ["FinalPack"]),
     ]
 )

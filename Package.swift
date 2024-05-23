57841438775f30236e01fc0cb221bb26e228f41b
Mike Donahue <development.md@gmail.com>
2024-05-23 14:11:04 -0400

    Update package

--- Package.swift
+++ Package.swift
@@ -15,14 +15,18 @@ let package = Package(
             targets: ["FinalPack"]),
     ],
     dependencies: [
-        .package(path: "../UI"),
-        .package(path: "../Core"),
-        .package(path: "../Foundation"),
+        .package(url: "https://github.com/MmikeDonahue/MikeUI.git", from: "1.0.0"),
+        .package(url: "https://github.com/MmikeDonahue/MikeCore.git", from: "1.0.0"),
+        .package(url: "https://github.com/MmikeDonahue/MikeFoundation.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "FinalPack",
-            dependencies: ["UI", "Core", "Foundation"]),
+            dependencies: [
+                .product(name: "MikeUI", package: "MikeUI"),
+                .product(name: "MikeCore", package: "MikeCore"),
+                .product(name: "MikeFoundation", package: "MikeFoundation"),
+            ]),
         .testTarget(
             name: "FinalPackTests",
             dependencies: ["FinalPack"]),

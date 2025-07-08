ba5ad0e89b8485544dbecc073fa623a91fbce257
Mike Donahue <development.md@gmail.com>
2025-07-08 07:26:02 -0400

    Create dummy workspace to play with

---
+++ SDKWrapper/SDKWrapper/SDKWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDKWrapperApp.swift
+//  SDKWrapper
+//
+//  Created by Mike Donahue on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SDKWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
